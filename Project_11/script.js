9a3517306f4add234c679f46389d7e857729e252
gopal-fs <gopalpinapathuni.fs@gmail.com>
2025-05-29 12:36:50 +0530

    Added Project_11

---
+++ Project_11/script.js
@@ -0,0 +1,27 @@
+
+const swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true, 
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
