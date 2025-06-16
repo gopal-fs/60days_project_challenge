35aedce8d2dd34425f20f30bd3effa5b1abd4369
gopal-fs <gopalpinapathuni.fs@gmail.com>
2025-06-16 11:41:04 +0530

    Added Project_29

---
+++ Project_29/script.js
@@ -0,0 +1,31 @@
+let speech= new SpeechSynthesisUtterance();
+let content=document.getElementById("content");
+let voices=[];
+
+const voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voiceSelect.innerHTML = ''; 
+
+    voices.forEach((voice, i) => {
+        const option = new Option(voice.name + ' (' + voice.lang + ')', i);
+        voiceSelect.add(option);
+    });
+};
+
+
+voiceSelect.addEventListener("change",()=>{
+    speech.voice=voices[voiceSelect.value];
+})
+
+document.querySelector("button").addEventListener("click",function(){
+    if(content.value===""){
+        speech.text="Please Enter something to Listen";
+        window.speechSynthesis.speak(speech)
+    }
+    speech.text=content.value;
+    window.speechSynthesis.speak(speech);
+})
