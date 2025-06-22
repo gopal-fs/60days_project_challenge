1ed6b294f86ea7296d1dacffb5aab523c57d3c28
gopal-fs <gopalpinapathuni.fs@gmail.com>
2025-06-22 12:18:42 +0530

    Added Project_35

--- Project_35/script.js
+++ Project_35/script.js
@@ -1,6 +1,8 @@
 function on(){
     document.querySelector(".image").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("cat").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
 }
 function off(){
     document.querySelector(".image").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("cat").src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
 }
