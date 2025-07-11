d13542868a584cd19c3a9e62c111c8d9b4c27c61
gopal-fs <gopalpinapathuni.fs@gmail.com>
2025-07-11 11:51:40 +0530

    Added Project_46

---
+++ Project_46/script.js
@@ -0,0 +1,47 @@
+let button=document.querySelector('button')
+
+let textElement=document.getElementById("text")
+const roles=['Full Stack Developer', 'React Developer', 'MERN Developer', 'Frontend Engineer']
+let wordIndex=0 
+let charIndex=0
+let isDeleting=false
+
+
+
+button.onclick=function(){
+    document.querySelector('.toast-container').style.display='block'
+   
+    document.querySelector('.sidebar').style.display='flex'
+    setTimeout(function(){
+
+        document.querySelector('.toast-container').style.display='none'
+
+    },2200)
+}
+
+
+function Typer(){
+    const currentWord= roles[wordIndex]
+    const letter=currentWord.substring(0,charIndex)
+
+    textElement.textContent=letter
+    if(!isDeleting && charIndex<currentWord.length){
+        charIndex++
+        setTimeout(Typer,150)
+    }
+    else if(isDeleting && charIndex>0){
+        charIndex--
+        setTimeout(Typer,100)
+    }
+    else{
+        isDeleting=!isDeleting
+        if(!isDeleting){
+            wordIndex=(wordIndex+1)%roles.length
+        }
+        setTimeout(Typer,1000)
+    }
+
+
+}
+
+Typer()
