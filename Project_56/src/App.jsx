6a614ecdbb8d7f59cfe0dc9d0a5d3b1e52e31303
gopal-fs <gopalpinapathuni.fs@gmail.com>
2025-07-21 20:22:38 +0530

    Added Project_56

---
+++ Project_56/src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useMemo, useRef, useState } from 'react'
+
+const App = () => {
+  
+
+  const [counter,setCounter]=useState(0)
+  const [number,setNumber]=useState(0)
+  const inputElement=useRef('')
+  const countRef=useRef(0)
+  
+
+
+  useEffect(()=>{
+    console.log('Use Effect Rendered!')
+
+  },[number])
+
+  const updateRef=()=>{
+    countRef.current=countRef.current+1
+  }
+
+  const onSubmit=()=>{
+    alert(inputElement.current.value)
+  }
+
+  const calculateCube=()=>{
+    console.log(number)
+  }
+
+
+  const result=useMemo(calculateCube,[counter])
+
+  
+
+
+
+  return (
+    <>
+
+
+    {/* Use State Method Logic */}
+    <h1>Counter : {counter}</h1>
+    <button onClick={()=>setCounter(prev=>prev+1)}>Increase</button>
+    <button onClick={()=>setCounter(prev=>prev-1)}>Decrease</button>
+
+
+    {/* Use Ref Hook Method */}
+    <h1>Ref Render Count : {countRef.current}</h1>
+    <button onClick={updateRef}>Increase</button>
+
+    {/* Accessing DOM Elelements Using  Use Ref Hook Method */}
+
+    <input type='text'  ref={inputElement}  />
+    <button onClick={onSubmit}>Submit</button>
+
+   {/* Use Memo Hook Method */}
+   
+   
+
+    </>
+  )
+}
+
+export default App
