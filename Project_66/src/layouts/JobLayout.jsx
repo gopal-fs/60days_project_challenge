417dd95ed5d6ae49c135a95abdae1e9c6ec35be7
gopal-fs <gopalpinapathuni.fs@gmail.com>
2026-01-11 20:27:11 +0530

    Implemented Hooks in React

--- Project_66/src/layouts/JobLayout.jsx
+++ Project_66/src/layouts/JobLayout.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Jobs from '../pages/Jobs'
+import React, { useMemo, useState } from "react";
+import { Outlet } from "react-router-dom";
 
 const JobLayout = () => {
+  const [number, setNumber] = useState(0);
+
+  const cube = (num) => {
+    console.log("Done");
+    return Math.pow(num, 3);
+  };
+
+  const res = useMemo(() => cube(number), [number]);
+
   return (
     <div>
+      <h1>My Result: {res}</h1>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
       <h1>Welcome to Jobs Page</h1>
-      <p>Lis of current opening are follows</p>
-      
+      <p>List of current openings are as follows</p>
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default JobLayout
+export default JobLayout;
